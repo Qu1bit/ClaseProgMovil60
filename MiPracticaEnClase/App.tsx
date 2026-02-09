3a1bd6509a1e947e3f786fb1d9ebe893601029ea
DESKTOP-RC33L5C\Cristhian <joan15arita@gmail.com>
2026-02-08 19:21:51 -0600

    Personalizacion completa de boton input

--- MiPracticaEnClase/App.tsx
+++ MiPracticaEnClase/App.tsx
@@ -1,8 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import CustomButton from './src/components/CustomButton';
+import CustomInput from './src/components/CustomInput';
+import { useState } from 'react';
 
 export default function App() {
+  const [email, setEmail] =useState("cristhian@gmail.com");
+  const [password, setPassword] =useState("");
+  
   const handleOnLogin = () => {
     console.log("prueba login desde app");
     alert("Alerta login desde app");
@@ -12,32 +17,34 @@ export default function App() {
     alert("Alerta logout desde app");
   }
 
-  const handleForgot = () => {
-    alert("Olvidaste la contrasena?");
-  }
-
   return (
     <View style={styles.container}>
-      <Text>Open up App</Text>
-      <StatusBar style="auto" />
-      <View style={styles.buttonsWrapper}>
-        <View style={styles.card}>
-        <CustomButton 
-            title={'Login'} 
-            onClick={handleOnLogin} 
-            />
-        <CustomButton 
-            title={'Salir'}
-            onClick={handleOnLogout}
-            variant={'secondary'} 
-            />
-           <CustomButton
-            title={'Olvidaste la contrasena?'}
-            onClick={handleForgot}
-            variant={'tertiary'}        
-        />
-        </View>
+      <View style={styles.card}>
+        <Text>Open up App</Text>
+        <StatusBar style="auto" />
+        <View style={styles.buttonsWrapper}>
+          <CustomButton 
+              title={'Login'} 
+              onClick={handleOnLogin}
+              />
+          <CustomButton 
+              title={'Salir'}
+              onClick={handleOnLogout}
+              variant={'secondary'} />
 
+              <CustomInput 
+              placeholder={'Ingrese su correo'} 
+              onChange={setEmail} 
+              value={email} 
+              typeInput={'email'}              
+              />
+               <CustomInput 
+              placeholder={'Password'} 
+              onChange={setPassword} 
+              value={password} 
+              typeInput={'password'}              
+              />
+        </View>
       </View>
     </View>
   );
@@ -49,20 +56,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  card:{
+  card: {
     width: "80%",
     height: "80%",
-    borderRadius: 15,
-    backgroundColor:'#f3f4f6',
-    alignItems: "center",
-    justifyContent: "space-around",
+    borderRadius:15,
+    backgroundColor: '#f3f4f6',
   },
-  buttonsWrapper:{
-    
+  buttonsWrapper: {
     marginTop: 15,
-    width: "80%",
     height: "30%",
     alignItems: "center",
-
+    justifyContent: "space-around",
   },
 });
---
+++ MiPracticaEnClase/src/components/CustomInput.tsx
@@ -0,0 +1,72 @@
+import { TextInput, View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
+import { useState } from "react";
+
+type Props = {
+    placeholder: string,
+    onChange: (text: string) => void;
+    value: string;
+    typeInput : 'password' | 'email' | 'numeric' | 'text';
+    
+}
+export default function CustomInput ({placeholder, onChange, value, typeInput}:Props){
+//uso de variables en el estado local
+        //sintaxis:
+        //[nombreDeVariable, funcion] = useState(<valorInicial>);
+    const [isSecureText, setIsSecureText] = useState(typeInput === 'password'); 
+    const isPasswordField = typeInput === 'password';
+    const icon : typeof MaterialIcons["name"] | undefined = 
+        typeInput === "email" ? "email" : 
+            typeInput == "password" ? "lock" : undefined
+    
+    return(
+        //wrapper
+        <View style={styles.wrapper}>
+            {/* //inputContainer */}
+            <View style={styles.inputContainer}>
+                <MaterialIcons 
+                    name={icon}
+                    size={20}
+                    color={"#000000"}
+                />
+                <TextInput 
+                style={styles.input}
+                    placeholder={placeholder}
+                    value={value} 
+                    onChangeText={onChange}
+                    secureTextEntry={isSecureText}
+                />
+                {isPasswordField && 
+                    <TouchableOpacity onPress={()=>{setIsSecureText(!isSecureText)}}>
+                        <Ionicons name={isSecureText ? "eye" : "eye-off"}  size={20} />
+                    </TouchableOpacity>
+                }
+            
+            </View>
+            <Text>*Campo Requerido</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    wrapper:{
+        marginBottom:10,
+        width: "100%",
+        paddingHorizontal: 25,
+    },
+    inputContainer:{
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent:'space-around',
+        
+        borderWidth: 1,
+        borderColor: "#ccc",
+        borderRadius: 8,
+        paddingHorizontal: 13,
+    },
+    input :{
+        paddingHorizontal: 10,
+        width: '80%'
+
+    }
+});
